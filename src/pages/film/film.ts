8b8dcacbb8f6243a420239883bfc1eeccad8b4e8
ELINEAUAnthony <anthony.elineau@hotmail.fr>
2018-12-18 13:29:20 +0100

    infiniteScroll

--- src/pages/film/film.ts
+++ src/pages/film/film.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DetailPage } from '../detail/detail';
 import { MovieApiProvider } from '../../providers/movie-api/movie-api';
 
+
 interface IMovie {
   id: number;
   poster_path: string;
@@ -20,24 +21,40 @@ export class FilmPage {
   film:any;
   movies=[];
   myFilm;
+  page=1
   constructor(public navCtrl: NavController, public navParams: NavParams,public movieApiProvider: MovieApiProvider) {
   }
 
 
   ionViewDidLoad() {
   
-    this.movieApiProvider.getFilm().subscribe(
+    this.movieApiProvider.getFilm(this.page).subscribe(
       data => {
+        
           
           this.movies = data.results;
    })
    
-   
+   console.log(this.movies)
   }
 
   goToDetail(movie:IMovie){
     this.navCtrl.push(DetailPage, movie);
   }
+  doInfinite(infiniteScroll) {
+    this.page++;
+    setTimeout(() => {
+      this.movieApiProvider.getFilm(this.page).subscribe((data => {
+        let newlist = data.results;
+        newlist.forEach((movie)=>{
+          this.movies.push(movie);
+        });
+      }));
+      infiniteScroll.complete();
+    }, 500);
+  }
+
+
 }
 
 
--- src/providers/movie-api/movie-api.ts
+++ src/providers/movie-api/movie-api.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs/Observable';
 export class MovieApiProvider {
   private Url = 'https://api.themoviedb.org/3/movie/popular?'
 
+  Page = 1
+
 
   private httpOptions = {
     headers : new HttpHeaders({
@@ -15,13 +17,15 @@ export class MovieApiProvider {
     params:{
       "api_key" : "3af9c524dd7e2699f76bded125a10ba9",
       "language": "fr-FR",
-      "Popular": "string"
+      "page": this.Page.toString()
     }
   }
   constructor(public http: HttpClient) {
     console.log('Hello MovieApiProvider Provider');
   }
-  public getFilm():Observable<any>{
-    return this.http.get<any>(this.Url ,this.httpOptions);
+  public getFilm(iDpage):Observable<any>{
+    this.httpOptions.params.page = iDpage.toString()
+    return this.http.get(this.Url ,this.httpOptions);
   }
+
 }
